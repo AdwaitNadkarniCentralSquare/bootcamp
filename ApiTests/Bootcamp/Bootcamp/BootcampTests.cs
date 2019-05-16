b7b5100af286184e4942971aacdc43b56d7c243b
Adwait Nadkarni <adwait.nadkarni@aptean.com>
2019-05-16 16:08:50 -0700

    Api tests

---
+++ ApiTests/Bootcamp/Bootcamp/BootcampTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace RestSharpExample
+{
+
+    [TestClass]
+    public class Weather
+    {
+        [TestMethod]
+        public void GetWeatherInfoKelowna()
+        {
+            string city = "Kelowna";
+
+            //Creating Client connection	
+            RestClient restClient = new RestClient("http://restapi.demoqa.com/utilities/weather/city/");
+
+            //Creating request to get data from server
+            RestRequest restRequest = new RestRequest(city, Method.GET);
+
+            // Executing request to server and checking server response to the it
+            IRestResponse restResponse = restClient.Execute(restRequest);
+
+            // Extracting output data from received response
+            string response = restResponse.Content;
+
+            // Verifiying reponse
+            if (!response.Contains(city))
+                Assert.Fail("There should be information here");
+
+        }
+        [TestMethod]
+        public void GetWeatherInfoBlah()
+        {
+            string city = "Blah";
+
+            //Creating Client connection	
+            RestClient restClient = new RestClient("http://restapi.demoqa.com/utilities/weather/city/");
+
+            //Creating request to get data from server
+            RestRequest restRequest = new RestRequest(city, Method.GET);
+
+            // Executing request to server and checking server response to the it
+            IRestResponse restResponse = restClient.Execute(restRequest);
+
+            // Extracting output data from received response
+            string response = restResponse.Content;
+
+            // Verifiying reponse
+            if (response.Contains(city))
+                Assert.Fail("There should be no city information here.");
+
+        }
+
+    }
+}
---
+++ ApiTests/Bootcamp/Bootcamp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bootcamp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bootcamp")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1bda4d88-d9d4-4814-a7fa-aeab8126539c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
